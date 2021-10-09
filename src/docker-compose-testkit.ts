58663143d3e7cb08e74581e6295202d891e76dfa
Tomer <tomer@corevo.io>
2021-10-09 14:27:24 +0300

    getAddressForService

--- src/docker-compose-testkit.ts
+++ src/docker-compose-testkit.ts
@@ -1,3 +1,55 @@
-export function getAddressForService() {
-  return undefined
+import retry from 'p-retry'
+import fetch from 'node-fetch'
+import {getAddressForService as dctGetAddressForService} from '@corevo/docker-compose-mocha'
+
+const serviceAddressCache = new Map()
+
+export async function getAddressForService(
+  envName: string,
+  composePath: string,
+  serviceName: string,
+  exposedPort: number,
+  {
+    customHealthCheck = undefined,
+    fiveHundedStatusIsOk = false,
+  }: {
+    customHealthCheck?: (address: string) => Promise<boolean>
+    fiveHundedStatusIsOk?: boolean
+  } = {},
+  maxRetries = 10,
+) {
+  const serviceAddressKey = JSON.stringify({envName, composePath, serviceName, exposedPort})
+  const possibleAddress = serviceAddressCache.get(serviceAddressKey)
+  if (possibleAddress) {
+    return possibleAddress
+  }
+
+  const address = await retry(
+    async () => {
+      const address = await dctGetAddressForService(envName, composePath, serviceName, exposedPort)
+
+      if (customHealthCheck) {
+        await customHealthCheck(address)
+        return address
+      }
+      const response = await fetch(`http://${address}/`)
+
+      if (response.status >= (fiveHundedStatusIsOk ? 600 : 500)) {
+        throw new Error(`Failed to access ${serviceName}. Got status ${response.status}`)
+      }
+      return address
+    },
+    {retries: maxRetries, maxTimeout: 1000},
+  )
+
+  /*
+  if (err) {
+    const log = await getLogsForService(envName, composePath, serviceName)
+    throw err
+  }
+  */
+
+  serviceAddressCache.set(serviceAddressKey, address)
+
+  return address
 }
