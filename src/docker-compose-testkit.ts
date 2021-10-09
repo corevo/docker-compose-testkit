0c8ee17ca73da2464b3d3fcf642b0a780d2346ba
Tomer <tomer@corevo.io>
2021-10-09 14:37:27 +0300

    getInternalIpForService

--- src/docker-compose-testkit.ts
+++ src/docker-compose-testkit.ts
@@ -1,5 +1,6 @@
 import retry from 'p-retry'
 import fetch from 'node-fetch'
+import execa from 'execa'
 import {getAddressForService as dctGetAddressForService} from '@corevo/docker-compose-mocha'
 
 const serviceAddressCache = new Map()
@@ -52,4 +53,23 @@ export async function getAddressForService(
   serviceAddressCache.set(serviceAddressKey, address)
 
   return address
+}
+
+export async function getInternalIpForService(
+  envName: string,
+  composePath: string,
+  serviceName: string,
+) {
+  const {stdout} = await execa('docker-compose', [
+    '-p',
+    envName,
+    '-f',
+    `"${composePath}"`,
+    'exec',
+    '-T',
+    serviceName,
+    'hostname',
+    '-i',
+  ])
+  return stdout.replace('\n', '')
 }
